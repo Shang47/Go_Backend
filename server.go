e894f25b10ccd8292794e19c38bfb375dbd25691
Connor Shang <jim90101@gmail.com>
2026-08-07 15:30:50 +0800

    Refactor architecture in server.go, add stub for PlayerStore in server_test.go
    1. Create interface PlayerStore for func GetPlayerStore(), refactor PlayerServer to a struct, and reflect the changes
        to the relevant calls in server_test.go and main.go
    2. Create a stub for PlayerStore which use map to store the players' score and provide it to PlayerServer in
        server_test.go

--- server.go
+++ server.go
@@ -6,18 +6,16 @@ import (
 	"strings"
 )
 
-func PlayerServer(w http.ResponseWriter, r *http.Request) {
+type PlayerServer struct {
+	store PlayerStore
+}
+
+type PlayerStore interface {
+	GetPlayerScore(name string) int
+}
+
+func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	player := strings.TrimPrefix(r.URL.Path, "/players/")
 
-	fmt.Fprint(w, GetPlayerScore(player))
-}
-func GetPlayerScore(name string) string {
-	if name == "Pepper" {
-		return "20"
-	}
-
-	if name == "Floyd" {
-		return "10"
-	}
-	return ""
+	fmt.Fprint(w, p.store.GetPlayerScore(player))
 }
