583512069bb72576e8a0232e13d456cc4611251f
Connor Shang <jim90101@gmail.com>
2026-08-09 16:16:23 +0800

    Create stub for PlayerStore, Add test case for non-exist player, Modify the logic of writing a http response header
    1. (main.go) The execution of the previous build file failed due to the lack of PlayerStore in the server construct.
        Therefore, create a stub for PlayerStore which is implemented in memory to temporarily fix the problem.
        This will later be replaced by the real PlayerStore in the storage.
    2. (server_test.go) Add a test case for the scenario which the search player is not found. The implementation is by checking the http
        response header. This expose the problem that the previous test cases didn't check the response header. As a result,
        a helper function for asserting the response header is created.
    3.  (server.go) Add a logic for writing a "Not Found" status code based on the return score of GetPlayerScore()

--- server_test.go
+++ server_test.go
@@ -30,6 +30,7 @@ func TestGetPlayers(t *testing.T) {
 
 		server.ServeHTTP(response, request)
 
+		assertStatus(t, response.Code, http.StatusOK)
 		assertResponseBody(t, response.Body.String(), "20")
 	})
 	t.Run("returns Floyd's score", func(t *testing.T) {
@@ -38,8 +39,23 @@ func TestGetPlayers(t *testing.T) {
 
 		server.ServeHTTP(response, request)
 
+		assertStatus(t, response.Code, http.StatusOK)
 		assertResponseBody(t, response.Body.String(), "10")
 	})
+	t.Run("returns 404 on missing players", func(t *testing.T) {
+		request := newGetScoreRequest("Apollo")
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		assertStatus(t, response.Code, http.StatusNotFound)
+	})
+}
+func assertStatus(t testing.TB, got, want int) {
+	t.Helper()
+	if got != want {
+		t.Errorf("did not get correct status, got %d, want %d", got, want)
+	}
 }
 func newGetScoreRequest(name string) *http.Request {
 	req, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/players/%s", name), nil)
