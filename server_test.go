6a9f7a2e806e35a4c929e644a7e50045b572d046
Connor Shang <jim90101@gmail.com>
2026-08-12 16:55:26 +0800

    Add test case for record player's win, Refactor ServeHTTP() program logic
    1. (server_test.go) Add a test case for the functionality of storing a player's win through sending a HTTP Post request
    2. (server.go) Refactor the logic in ServeHTTP() to make the intention clearer

--- server_test.go
+++ server_test.go
@@ -51,6 +51,21 @@ func TestGetPlayers(t *testing.T) {
 		assertStatus(t, response.Code, http.StatusNotFound)
 	})
 }
+func TestStoreWins(t *testing.T) {
+	store := StubPlayerStore{
+		map[string]int{},
+	}
+	server := &PlayerServer{&store}
+
+	t.Run("it returns accepted on POST", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodPost, "/players/Pepper", nil)
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		assertStatus(t, response.Code, http.StatusAccepted)
+	})
+}
 func assertStatus(t testing.TB, got, want int) {
 	t.Helper()
 	if got != want {
