aa841aa3ea440dcee32d1f5c75137a314a77a7ed
Connor Shang <jim90101@gmail.com>
2026-08-28 14:28:23 +0800

    fix: Add mutex for read and write to InMemoryPlayerStore

--- in_memory_player_store.go
+++ in_memory_player_store.go
@@ -1,17 +1,26 @@
 package main
 
+import (
+	"sync"
+)
+
 func NewInMemoryPlayerStore() *InMemoryPlayerStore {
-	return &InMemoryPlayerStore{map[string]int{}}
+	return &InMemoryPlayerStore{store: map[string]int{}}
 }
 
 type InMemoryPlayerStore struct {
 	store map[string]int
+	mu    sync.Mutex
 }
 
 func (i *InMemoryPlayerStore) RecordWin(name string) {
+	i.mu.Lock()
+	defer i.mu.Unlock()
 	i.store[name]++
 }
 func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	i.mu.Lock()
+	defer i.mu.Unlock()
 	return i.store[name]
 }
 func (i *InMemoryPlayerStore) close() {}
