05cef514adfa6b01312c9e2b769a7123f3c930c3
Connor Shang <jim90101@gmail.com>
2026-08-30 11:26:11 +0800

    test(server_integration_test): Add test case for getting league fix(in_memory_player_store): Implement GetLeague() to actually return a list of players

--- server_integration_test.go
+++ server_integration_test.go
@@ -7,21 +7,34 @@ import (
 )
 
 func TestRecordingWinsAndRetrievingThem(t *testing.T) {
-	t.Run("test with in memory player store", func(t *testing.T) {
-		store := NewInMemoryPlayerStore()
-		server := NewPlayerServer(store)
-		player := "Pepper"
+	//t.Run("test with in memory player store", func(t *testing.T) {
+	store := NewInMemoryPlayerStore()
+	server := NewPlayerServer(store)
+	player := "Pepper"
 
-		server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
-		server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
-		server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
+	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
+	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
+	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
 
+	t.Run("get score", func(t *testing.T) {
 		response := httptest.NewRecorder()
 		server.ServeHTTP(response, newGetScoreRequest(player))
 		assertStatus(t, response.Code, http.StatusOK)
 
 		assertResponseBody(t, response.Body.String(), "3")
 	})
+	t.Run("get league", func(t *testing.T) {
+		response := httptest.NewRecorder()
+		server.ServeHTTP(response, newLeagueRequest())
+		assertStatus(t, response.Code, http.StatusOK)
+
+		got := getLeagueFromResponse(t, response.Body)
+		want := []Player{
+			{"Pepper", 3},
+		}
+		assertLeague(t, got, want)
+	})
+	//})
 	/*t.Run("test with player store in MariaDB", func(t *testing.T) {
 		store := NewMariaPlayerStore()
 		server := PlayerServer{store}
