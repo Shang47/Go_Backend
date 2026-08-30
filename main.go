146ea19c57a4389a370913bf805b5feb8e262175
Connor Shang <jim90101@gmail.com>
2026-08-30 15:39:35 +0800

    refactor(main): refactor the control flow

--- main.go
+++ main.go
@@ -7,6 +7,10 @@ import (
 
 func main() {
 	server := NewPlayerServer(NewMariaPlayerStore())
-	log.Fatal(http.ListenAndServe(":5000", server))
+
+	if err := http.ListenAndServe(":5000", server); err != nil {
+		log.Fatalf("could not listen on port 5000 %v", err)
+	}
+
 	defer server.store.close()
 }
