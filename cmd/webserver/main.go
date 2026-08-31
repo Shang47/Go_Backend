56b617171a37aa1924a41507ae7385367c82ede4
Connor Shang <jim90101@gmail.com>
2026-08-31 10:53:08 +0800

    refactor(main): Import package 'poker' and temporally comment out function call to close DB
    1. The function call refers to a object which is not exported. (Need to be fixed later)

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -3,14 +3,16 @@ package main
 import (
 	"log"
 	"net/http"
+
+	poker "github.com/Shang47/simple-web-server"
 )
 
 func main() {
-	server := NewPlayerServer(NewMariaPlayerStore())
+	server := poker.NewPlayerServer(poker.NewMariaPlayerStore())
 
 	if err := http.ListenAndServe(":5000", server); err != nil {
 		log.Fatalf("could not listen on port 5000 %v", err)
 	}
 
-	defer server.store.close()
+	//defer server.store.close()
 }
