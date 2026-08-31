9ec60059e663c8d6eefc890a39af2011595290f9
Connor Shang <jim90101@gmail.com>
2026-08-31 16:42:15 +0800

    feat(cli/main): Import package 'poker' and implement the functionality

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	poker "github.com/Shang47/simple-web-server"
 )
 
 func main() {
 	fmt.Println("Let's play poker.")
+	fmt.Println("Type {Name} wins to record a win")
+
+	store := poker.NewMariaPlayerStore()
+
+	game := poker.CLI{store, os.Stdin}
+	game.PlayPoker()
 }
