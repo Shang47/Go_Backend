846286e416ba66109dc099bbb9da2462da524499
Connor Shang <jim90101@gmail.com>
2026-08-31 14:43:21 +0800

    refactor(server_test): extract the assertions to verify whether the recorded data is correct or not into a helper function refactor(CLI_test): Use the helper function instead

--- server_test.go
+++ server_test.go
@@ -83,14 +83,19 @@ func TestStoreWins(t *testing.T) {
 
 		assertStatus(t, response.Code, http.StatusAccepted)
 
-		if len(store.winCalls) != 1 {
-			t.Fatalf("got %d calls to RecordWin want %d", len(store.winCalls), 1)
-		}
-
-		if store.winCalls[0] != player {
-			t.Errorf("did not store correct winner got %q want %q", store.winCalls[0], player)
-		}
+		assertPlayerWin(t, &store, player)
 	})
+}
+func assertPlayerWin(t testing.TB, store *StubPlayerStore, winner string) {
+	t.Helper()
+
+	if len(store.winCalls) != 1 {
+		t.Fatalf("got %d calls to RecordWin want %d", len(store.winCalls), 1)
+	}
+
+	if store.winCalls[0] != winner {
+		t.Errorf("did not store correct winner got %q want %q", store.winCalls[0], winner)
+	}
 }
 func TestLeague(t *testing.T) {
 
