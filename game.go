3ed052593892b077983fca483970c59bf6f1da5c
Connor Shang <jim90101@gmail.com>
2026-09-04 15:17:03 +0800

    refactor(game): Create a game interface and rename old stuff from 'game' to 'TexasHoldem' in order to decouple CLI and concrete game refactor(CLI): Reflect the change to the old references to *Game

--- game.go
+++ game.go
@@ -4,12 +4,16 @@ import (
 	"time"
 )
 
-type Game struct {
+type Game interface {
+	Start(numberOfPlayers int)
+	Finish(winner string)
+}
+type TexasHoldem struct {
 	alerter BlindAlerter
 	store   PlayerStore
 }
 
-func (p *Game) Start(numberOfPlayers int) {
+func (p *TexasHoldem) Start(numberOfPlayers int) {
 	blindIncrement := time.Duration(5+numberOfPlayers) * time.Minute
 
 	blinds := []int{100, 200, 300, 400, 500, 600, 800, 1000, 2000, 4000, 8000}
@@ -21,11 +25,11 @@ func (p *Game) Start(numberOfPlayers int) {
 	}
 }
 
-func (p *Game) Finish(winner string) {
+func (p *TexasHoldem) Finish(winner string) {
 	p.store.RecordWin(winner)
 }
-func NewGame(alerter BlindAlerter, store PlayerStore) *Game {
-	return &Game{
+func NewGame(alerter BlindAlerter, store PlayerStore) *TexasHoldem {
+	return &TexasHoldem{
 		alerter: alerter,
 		store:   store,
 	}
