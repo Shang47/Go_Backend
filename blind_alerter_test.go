0b845e25914d3faca6f842998553e775094ca676
Connor Shang <jim90101@gmail.com>
2026-09-08 11:10:06 +0800

    refactor(blind_alerter_test): Update the test to use synctest package and synctest.Wait() to prevent data race

--- blind_alerter_test.go
+++ blind_alerter_test.go
@@ -3,19 +3,23 @@ package poker
 import (
 	"bytes"
 	"testing"
+	"testing/synctest"
 	"time"
 )
 
 func TestStdOutAlerter(t *testing.T) {
-	out := &bytes.Buffer{}
-	alerter := StdOutAlerter(out)
+	synctest.Test(t, func(t *testing.T) {
+		out := &bytes.Buffer{}
+		alerter := StdOutAlerter(out)
 
-	alerter.ScheduleAlertAt(5*time.Second, 100)
+		alerter.ScheduleAlertAt(5*time.Second, 100)
 
-	time.Sleep(6 * time.Second)
+		time.Sleep(6 * time.Second)
+		synctest.Wait()
 
-	want := "Blind is now 100"
-	if out.String() != want {
-		t.Errorf("got %q, ant %q", out.String(), want)
-	}
+		want := "Blind is now 100"
+		if out.String() != want {
+			t.Errorf("got %q, ant %q", out.String(), want)
+		}
+	})
 }
