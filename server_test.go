4572d5434a799f8d381bd17366b1b55f6930f5af
Connor Shang <jim90101@gmail.com>
2026-09-08 17:12:41 +0800

    refactor(server_test): Add a helper function for generating a new game request

--- server_test.go
+++ server_test.go
@@ -94,11 +94,18 @@ func TestGame(t *testing.T) {
 	t.Run("GET /game returns 200", func(t *testing.T) {
 		server := NewPlayerServer(&StubPlayerStore{})
 
-		request, _ := http.NewRequest(http.MethodGet, "/game", nil)
+		request := newGameRequest()
 		response := httptest.NewRecorder()
 
 		server.ServeHTTP(response, request)
 
 		AssertStatus(t, response.Code, http.StatusOK)
 	})
+}
+func newGameRequest() *http.Request {
+	req, err := http.NewRequest(http.MethodGet, "/game", nil)
+	if err != nil {
+		return nil
+	}
+	return req
 }
