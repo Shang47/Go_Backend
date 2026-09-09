d4d801b6efef1f4b2b17e5442e06f6975b5ec003
Connor Shang <jim90101@gmail.com>
2026-09-09 16:49:56 +0800

    feat(websocket): Add a struct of playerServerWS to handle creating websocket connection and reading message from it refactor(server): Use the newly created constructor and method in websocket method

--- server.go
+++ server.go
@@ -99,12 +99,12 @@ var wsUpgrader = websocket.Upgrader{
 }
 
 func (p *PlayerServer) webSocket(w http.ResponseWriter, r *http.Request) {
-	conn, _ := wsUpgrader.Upgrade(w, r, nil)
+	ws := newPlayerServerWS(w, r)
 
-	_, numberOfPlayersMsg, _ := conn.ReadMessage()
-	numberOfPlayers, _ := strconv.Atoi(string(numberOfPlayersMsg))
+	numberOfPlayersMsg := ws.WaitForMsg()
+	numberOfPlayers, _ := strconv.Atoi(numberOfPlayersMsg)
 	p.game.Start(numberOfPlayers, io.Discard) //todo: Don't discard the blinds messages!
 
-	_, winnerMsg, _ := conn.ReadMessage()
-	p.game.Finish(string(winnerMsg))
+	winner := ws.WaitForMsg()
+	p.game.Finish(winner)
 }
