8ff322b02f2596927c3efaafa0ae1afeb60dd9de
Connor Shang <jim90101@gmail.com>
2026-09-09 13:55:36 +0800

    refactor(CLI_test): Refactor a test case

--- CLI_test.go
+++ CLI_test.go
@@ -53,14 +53,14 @@ func userSends(messages ...string) io.Reader {
 func TestCLI(t *testing.T) {
 	t.Run("start game with 3 players and finish game with 'Chris' as winner", func(t *testing.T) {
 		game := &GameSpy{}
-		stdout := &bytes.Buffer{}
 
+		out := &bytes.Buffer{}
 		in := userSends("3", "Chris wins")
-		cli := poker.NewCLI(in, stdout, game)
 
+		cli := poker.NewCLI(in, out, game)
 		cli.PlayPoker()
 
-		assertMessagesSentToUser(t, stdout, poker.PlayerPrompt)
+		assertMessagesSentToUser(t, out, poker.PlayerPrompt)
 		assertGameStartedWith(t, game, 3)
 		assertFinishCalledWith(t, game, "Chris")
 	})
